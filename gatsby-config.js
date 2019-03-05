4766260f535724a5bdd461aa0a3e5cc3fc9f3f76
Richard Schave <schavester@gmail.com>
2019-03-04 18:03:09 -0800

    gatsby wordpress commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,57 +1,88 @@
-require('dotenv').config({
-  path: '.env',
-});
-const path = require('path');
+const config = require('./data/SiteConfig')
 
-const siteUrl = 'https://wordpress-gatsby.netlify.com/';
+const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix
+
 module.exports = {
+  pathPrefix: config.pathPrefix,
   siteMetadata: {
-    siteUrl,
-    description: 'Convert your wordpress blog to pwa using gatsby',
-    keyword: 'wordpress, gatsby, blog, pwa',
-    title: 'Wordpress Blog PWA',
+    siteUrl: config.siteUrl + pathPrefix,
+    rssMetadata: {
+      site_url: config.siteUrl + pathPrefix,
+      feed_url: config.siteUrl + pathPrefix + config.siteRss,
+      title: config.siteTitle,
+      description: config.siteDescription,
+      image_url: `${config.siteUrl + pathPrefix}/logos/logo-512.png`,
+      author: config.userName,
+      copyright: config.copyright
+    }
   },
   plugins: [
+    'gatsby-plugin-styled-components',
+    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-wordpress',
       options: {
-        baseUrl: 'insroland.hiddenhistoryblogs.com',
+        // The base url to your WP site.
+        baseUrl: 'gatsbynetliflydemo.justinwhall.com',
+        // baseUrl: 'wpgatsby.wtf',
+        // WP.com sites set to true, WP.org set to false
+        hostingWPCOM: false,
+        // The protocol. This can be http or https.
         protocol: 'https',
-        hostingWPCOM: true,
-        useACF: true,
-        auth: {
-          wpcom_app_clientSecret: process.env.WORDPRESS_SECRET,
-          wpcom_app_clientId: process.env.WORDPRESS_CLIENTID,
-          wpcom_user: process.env.WORDPRESS_U,
-          wpcom_pass: process.env.WORDPRESS_P,
-        },
-        verboseOutput: false,
-      },
+        // Use 'Advanced Custom Fields' Wordpress plugin
+        useACF: false,
+        auth: {},
+        // Set to true to debug endpoints on 'gatsby build'
+        verboseOutput: true,
+        excludedRoutes: [
+          "/*/*/comments",
+          "/yoast/**",
+          "/*/*/users",
+          "/*/users/me",
+          "/oembed/*"
+        ],
+      }
     },
-    'gatsby-plugin-react-helmet',
+    // {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     trackingId: config.googleAnalyticsID,
+    //   },
+    // },
+    {
+      resolve: 'gatsby-plugin-nprogress',
+      options: {
+        color: config.themeColor
+      }
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-twitter',
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'Wordpress Blog PWA',
-        short_name: 'Blog PWA',
-        start_url: '/blog',
-        background_color: '#1d69ab',
-        theme_color: '#1d69ab',
-        display: 'standalone',
-        icon: path.join(__dirname, 'src/images/logo.png'),
-      },
+        name: config.siteTitle,
+        short_name: config.siteTitle,
+        description: config.siteDescription,
+        start_url: config.pathPrefix,
+        background_color: config.backgroundColor,
+        theme_color: config.themeColor,
+        display: 'minimal-ui',
+        icons: [
+          {
+            src: '/logos/logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/logos/logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     },
-    'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        host: siteUrl,
-        sitemap: `${siteUrl}/sitemap.xml`,
-        policy: [{ userAgent: '*', disallow: '' }],
-      },
-    },
-    'gatsby-plugin-offline',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-  ],
-};
+    'gatsby-plugin-offline'
+  ]
+}
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,64 +1,161 @@
-const _ = require('lodash');
-const Promise = require('bluebird');
-const path = require('path');
-const slash = require('slash');
+const _ = require(`lodash`)
+const Promise = require(`bluebird`)
+const path = require(`path`)
+const slash = require(`slash`)
+const webpackLodashPlugin = require('lodash-webpack-plugin')
 
-// Implement the Gatsby API “createPages”. This is
-// called after the Gatsby bootstrap is finished so you have
-// access to any information necessary to programmatically
-// create pages.
+const DEPLOY_ENV  = process.env.DEPLOY_ENV || 'lbn_published_production';
+
+
+/**
+ * Generate node edges
+ *
+ * @param {any} { node, boundActionCreators, getNode }
+ */
+exports.onCreateNode = ({ node, boundActionCreators }) => {
+  const { createNodeField } = boundActionCreators;
+
+  if (!Object.prototype.hasOwnProperty.call(node, 'meta')) {
+    return;
+  }
+
+  let deploy;
+
+  if (node.meta[DEPLOY_ENV]) {
+    deploy = true;
+  } else {
+    deploy = false;
+  }
+
+  createNodeField({ node, name: 'deploy', value: deploy });
+};
+
 // Will create pages for Wordpress pages (route : /{slug})
-// Will create pages for Wordpress posts (route : /post/{slug})
-exports.createPages = ({
-  graphql,
-  actions,
-}) => {
-  const {
-    createPage,
-  } = actions;
-  return new Promise((resolve, reject) => {
-    // The “graphql” function allows us to run arbitrary
-    // queries against the local Wordpress graphql schema. Think of
-    // it like the site has a built-in database constructed
-    // from the fetched data that you can run queries against.
+// Will create pages for Wordpress posts (route : /{slug})
 
-    // ==== POSTS (WORDPRESS NATIVE AND ACF) ====
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators
+  return new Promise((resolve, reject) => {
+    // First, query all the pages on your WordPress
     graphql(
       `
+        {
+          allWordpressPage {
+            edges {
+              node {
+                id
+                slug
+                status
+                template
+                fields {
+                  deploy
+                }
+              }
+            }
+          }
+        }
+      `
+    )
+      .then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
+        }
+        // Create those pages with the wp_page.jsx template.
+        const pageTemplate = path.resolve(`./src/templates/wp_page.jsx`)
+        _.each(result.data.allWordpressPage.edges, edge => {
+          if (edge.node.fields.deploy) {
+            createPage({
+              path: `/${edge.node.slug}/`,
+              component: slash(pageTemplate),
+              context: {
+                id: edge.node.id
+              }
+            })
+          }
+        })
+      })
+      // Now, querying all wordpressPosts
+      .then(() => {
+        graphql(
+          `
             {
               allWordpressPost {
                 edges {
                   node {
                     id
                     slug
-                    status
-                    template
-                    format
+                    modified
+                    categories {
+                      name
+                    }
+                    fields {
+                      deploy
+                    }
                   }
                 }
               }
             }
-          `,
-    ).then((result) => {
-      if (result.errors) {
-        console.log(result.errors);
-        reject(result.errors);
-      }
-      const postTemplate = path.resolve('./src/templates/post.js');
-      // We want to create a detailed page for each
-      // post node. We'll just use the Wordpress Slug for the slug.
-      // The Post ID is prefixed with 'POST_'
-      _.each(result.data.allWordpressPost.edges, (edge) => {
-        createPage({
-          path: `/blog/${edge.node.slug}`,
-          component: slash(postTemplate),
-          context: {
-            id: edge.node.id,
-          },
-        });
-      });
-      resolve();
-    });
-    // ==== END POSTS ====
-  });
-};
+          `
+        ).then(result => {
+          if (result.errors) {
+            console.log(result.errors)
+            reject(result.errors)
+          }
+
+          const categories = []
+          const postTemplate = path.resolve(`./src/templates/post.jsx`)
+          // We want to create a detailed page for each
+          // post node. We'll just use the Wordpress Slug for the slug.
+          // The Post ID is prefixed with 'POST_'
+
+          _.each(result.data.allWordpressPost.edges, edge => {
+
+            if (edge.node.fields.deploy) {
+              // grab all the tags and categories for later use
+              edge.node.categories.forEach(category => {
+                categories.push(category.name)
+              })
+
+              createPage({
+                path: `/${edge.node.slug}`,
+                component: slash(postTemplate),
+                context: {
+                  id: edge.node.id,
+                }
+              })
+            }
+
+          })
+          // ==== END POSTS ====
+
+          // Create pages for each unique category
+
+          const categoriesTemplate = path.resolve(
+            `./src/templates/category.jsx`
+          )
+
+          const catSet = new Set(categories)
+
+          catSet.forEach(cat => {
+            createPage({
+              path: `/category/${_.kebabCase(cat)}/`,
+              component: slash(categoriesTemplate),
+              context: {
+                id: cat
+              }
+            })
+          })
+          resolve()
+        })
+      })
+    // === END TAGS ===
+    resolve()
+  })
+}
+
+exports.modifyWebpackConfig = ({ config, stage }) => {
+  if (stage === 'build-javascript') {
+    config.plugin('Lodash', webpackLodashPlugin, null)
+  }
+}
